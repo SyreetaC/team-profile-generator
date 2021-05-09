12fd67c9a62c9d27985ebad8a722479181513791
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-09 18:58:04 +0100

    generate manager fn

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const inquirer = require("inquirer");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 const fs = require("fs");
 const generateHTML = require("./src/utils/generateHTML");
 
+// Returns a new instance of the manager class with the prompted managerQuestions
+const generateManager = async () => {
+  const managerQuestions = [
+    {
+      type: "input",
+      name: "managerName",
+      message: "Enter the manager's name:",
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "Enter the manager's employee ID:",
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "Enter the manager's e-mail address:",
+    },
+    {
+      type: "input",
+      name: "managerOfficeNumber",
+      message: "Enter the manager's office number:",
+    },
+  ];
+
+  const managerObject = await inquirer.prompt(managerQuestions);
+
+  return new Manager(
+    managerObject.managerName,
+    managerObject.managerId,
+    managerObject.managerEmail,
+    managerObject.managerOfficeNumber
+  );
+};
+
 const init = () => {
   console.log("started");
+  generateManager();
   generateHTML();
 };
 
