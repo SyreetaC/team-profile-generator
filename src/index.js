9c8b715fd373dc08e0eb9ca08989bc53a98ee903
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-09 12:37:14 +0100

    added init function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateHTML = require("./utils/generateHTML");
+
+const init = () => {
+  console.log("started");
+};
+
+init();
