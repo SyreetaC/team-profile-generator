963e0a6fe05410b4fbb4da65b88b37e9f797292f
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-16 19:36:00 +0100

    generatemanager function

--- src/utils/generateManager.js
+++ src/utils/generateManager.js
@@ -1,14 +1,31 @@
-const manager = require("../lib/Manager");
-const employee = require("../lib/Employee");
+const fs = require("fs");
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
 
 const generateManager = (team) => {
-  console.log(team);
-  team.map(generateManager);
+  const manager = team[0];
+  generateManagerCard(manager);
+};
 
-  //map through team array
-  //if statements based on getJobRole
-  //array.join of results of if statements
-  //create functions for different employee cards
+const generateManagerCard = (manager) => {
+  return `<div class="col">
+  <div class="card text-white bg-primary m-3" style="max-width: 18rem">
+    <div class="card-header">
+      <h5 class="card-title">${manager.name}</h5>
+      <h5 class="card-title">${manager.getJobRole}</h5>
+    </div>
+    <div class="card">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${manager.id}</li>
+        <li class="list-group-item">
+          E-mail:
+          <a href="mary@manager.com">${manager.email}</a>
+        </li>
+        <li class="list-group-item">Office number: ${manager.officeNumber}</li>
+      </ul>
+    </div>
+  </div>
+</div>`;
 };
 
 module.exports = generateManager;
