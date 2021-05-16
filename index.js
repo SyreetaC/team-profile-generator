5b6a5c4c1703145312b86958b91b33397ff8a7d7
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-16 20:17:19 +0100

    removed comment

--- index.js
+++ index.js
@@ -108,7 +108,6 @@ const generateIntern = async () => {
 };
 
 // function to create an employees array
-//TO BE COMPLETED
 const generateEmployees = async () => {
   const teamArray = [];
   const employeeChoiceQuestions = [
