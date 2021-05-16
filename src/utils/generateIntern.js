91bafb23d4ea14d63db8cc5d591ac967f6845751
Suraj Verma <surajverma@rentalcars.com>
2021-05-16 21:16:58 +0100

    fix code

--- src/utils/generateIntern.js
+++ src/utils/generateIntern.js
@@ -1,19 +1,8 @@
-const Intern = require("../lib/Intern");
-
-const generateIntern = (team) => {
-  const intern = team.map(generateInternCard);
-  if (intern === Intern) {
-    console.log("there is an intern!");
-  }
-  generateInternCard(intern);
-};
-
-const generateInternCard = (intern) => {
-  return `<div class="col">
-  <div class="card text-white bg-primary m-3" style="max-width: 18rem">
+const generateIntern = (intern) => {
+  return `<div class="card text-white bg-primary m-3" style="max-width: 18rem">
     <div class="card-header">
       <h5 class="card-title">${intern.name}</h5>
-      <h5 class="card-title">${intern.getJobRole}</h5>
+      <h5 class="card-title">${intern.getJobRole()}</h5>
     </div>
     <div class="card">
       <ul class="list-group list-group-flush">
@@ -23,13 +12,11 @@ const generateInternCard = (intern) => {
           <a href="john@intern.com">${intern.email}</a>
         </li>
         <li class="list-group-item">
-          School: ${intern.getSchool}
+          School: ${intern.getSchool()}
         </li>
       </ul>
     </div>
-  </div>
-</div>
-</div>`;
+  </div>`;
 };
 
 module.exports = generateIntern;
