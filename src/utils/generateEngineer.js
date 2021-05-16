480cc399886e1aef4a7ffb47a5ae8b4ee01188e6
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-16 19:36:26 +0100

    generateEngineer function

--- src/utils/generateEngineer.js
+++ src/utils/generateEngineer.js
@@ -1,3 +1,20 @@
-const employee = require("../lib/Employee");
-const engineer = require("../lib/Engineer");
-const generateHTML = require("./generateHTML");
+const fs = require("fs");
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+
+const generateEngineer = (team) => {
+  const engineer = team.map(generateEngineerCard);
+  if (engineer === Engineer) {
+    console.log("there is an engineer!");
+  }
+  generateEngineerCard(engineer);
+};
+
+const generateEngineerCard = (engineer) => {};
+
+//map through team array
+//if statements based on getJobRole
+//array.join of results of if statements
+//create functions for different employee cards
+
+module.exports = generateEngineer;
