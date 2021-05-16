589a16250d781475bbaf382d2f6ee140a46f15a4
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-16 20:05:12 +0100

    engineer card

--- src/utils/generateEngineer.js
+++ src/utils/generateEngineer.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const Employee = require("../lib/Employee");
 const Engineer = require("../lib/Engineer");
 
+//map through team array
+//if statements based on getJobRole
+//array.join of results of if statements
+//create functions for different employee cards
+
 const generateEngineer = (team) => {
   const engineer = team.map(generateEngineerCard);
   if (engineer === Engineer) {
@@ -10,7 +15,31 @@ const generateEngineer = (team) => {
   generateEngineerCard(engineer);
 };
 
-const generateEngineerCard = (engineer) => {};
+const generateEngineerCard = (engineer) => {
+  return ` <div class="col">
+  <div class="card text-white bg-primary m-3" style="max-width: 18rem">
+    <div class="card-header">
+      <h5 class="card-title">${engineer.name}</h5>
+      <h5 class="card-title">${engineer.getJobRole}</h5>
+    </div>
+    <div class="card">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.id}</li>
+        <li class="list-group-item">
+          E-mail:
+          <a href="bob@engineer.com">${engineer.email}</a>
+        </li>
+        <li class="list-group-item">
+          gitHub account:
+          <a href="https://github.com/bob" , target="_blank"
+            >${engineer.getGitHub}</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>`;
+};
 
 //map through team array
 //if statements based on getJobRole
